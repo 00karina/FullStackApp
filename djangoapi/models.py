7a119b702adf735f6ab9043025169e0f0745e02d
karina shrestha <karinasrt00@gmail.com>
2021-09-03 13:39:03 +0545

    Adding Models

--- djangoapi/models.py
+++ djangoapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    DepartmentId=models.AutoField(primary_key=True)
+    DepartmentName=models.CharField(max_length=500)
+
+class Employee(models.Model):
+    EmployeetId=models.AutoField(primary_key=True)
+    EmployeeName=models.CharField(max_length=500)
+    Department=models.CharField(max_length=500)
+    DateofJoining=models.DateField()
+    PhotoName=models.CharField(max_length=500)
+
+
+
